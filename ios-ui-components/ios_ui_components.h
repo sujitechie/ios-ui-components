19d38acc711a7a818a70747a0a0afa94f00feb2e
sujith <psujith.92@gmail.com>
2022-11-01 10:35:47 +0530

    Initial Commit

---
+++ ios-ui-components/ios_ui_components.h
@@ -0,0 +1,18 @@
+//
+//  ios_ui_components.h
+//  ios-ui-components
+//
+//  Created by sujith on 01/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ios_ui_components.
+FOUNDATION_EXPORT double ios_ui_componentsVersionNumber;
+
+//! Project version string for ios_ui_components.
+FOUNDATION_EXPORT const unsigned char ios_ui_componentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_ui_components/PublicHeader.h>
+
+
